86723d3666eb1233ecf08d82f60d30df4b14a336
Rishabh7g <rishabh7g@gmail.com>
2024-08-06 15:13:15 +1000

    Setup jest

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const path = require('path');
+
+module.exports = {
+  preset: 'ts-jest',
+  rootDir: '.',
+  testEnvironment: 'jest-environment-jsdom',
+  displayName: 'frontend',
+  moduleDirectories: ['node_modules', 'src'],
+  setupFiles: ['<rootDir>/jest/jest.polyfills.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/__mocks__/style-mock.ts',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/file-mock.ts',
+    'src/(.*)$': '<rootDir>/src/$1',
+  },
+  coverageDirectory: path.join(__dirname, 'coverage/frontend'),
+  collectCoverageFrom: [
+    'src/components/**/*.{ts,tsx}',
+    '!src/components/**/*.{types,stories,test,spec,constant}.{ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+};
