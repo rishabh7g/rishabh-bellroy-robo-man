03e61bcdceb09c6890b1fe12e4bd48fb5108efe3
Rishabh7g <rishabh7g@gmail.com>
2024-08-06 16:22:53 +1000

    Update constant file

--- src/constants/direction.constant.ts
+++ src/constants/direction.constant.ts
@@ -1,7 +1,8 @@
 import { Direction } from 'src/types/types';
 
-export const LEFT_ROTATION = 3;
-export const RIGHT_ROTATION = 1;
+export const TOTAL_GRID_SIZE = 5;
+export const LEFT_ROTATION = 1;
+export const RIGHT_ROTATION = 3;
 export const OPPOSITE_ROTATION = 2;
 export const DIRECTIONS: Direction[] = [
   Direction.NORTH,
@@ -9,3 +10,4 @@ export const DIRECTIONS: Direction[] = [
   Direction.SOUTH,
   Direction.WEST,
 ];
+
