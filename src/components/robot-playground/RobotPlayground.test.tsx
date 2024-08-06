32721e972ec6109d84ef6c70e4223a8a1fc023fe
Rishabh7g <rishabh7g@gmail.com>
2024-08-06 16:22:53 +1000

    Update <RobotPlayground />
    - Add tests for <RobotPlayground />

---
+++ src/components/robot-playground/RobotPlayground.test.tsx
@@ -0,0 +1,73 @@
+import { render } from '@testing-library/react';
+import { RobotPlayground } from 'src/components/robot-playground/RobotPlayground';
+import { Direction } from 'src/types/types';
+
+const CELL_ROBOT_CLASS = 'cell__robot';
+const CELL_ROBOT_SELECTOR = `.${CELL_ROBOT_CLASS}`;
+const ROW_CLASS = 'row';
+const CELL_CLASS = 'cell';
+const INITIAL_COORDINATE_X = 0;
+const INITIAL_COORDINATE_Y = 0;
+
+describe('RobotPlayground Component', () => {
+  test('renders a 5x5 grid', () => {
+    const { container } = render(
+      <RobotPlayground
+        coordinateX={INITIAL_COORDINATE_X}
+        coordinateY={INITIAL_COORDINATE_Y}
+        direction={Direction.NORTH}
+      />
+    );
+
+    const rows = container.getElementsByClassName(ROW_CLASS);
+    expect(rows.length).toBe(5);
+
+    for (let row of rows) {
+      const cells = row.getElementsByClassName(CELL_CLASS);
+      expect(cells.length).toBe(5);
+    }
+  });
+
+  test('renders robot at the correct position', () => {
+    const coordinateX = 2;
+    const coordinateY = 3;
+
+    const { container } = render(
+      <RobotPlayground
+        coordinateX={coordinateX}
+        coordinateY={coordinateY}
+        direction={Direction.NORTH}
+      />
+    );
+
+    const robotCell = container.querySelector(CELL_ROBOT_SELECTOR);
+    expect(robotCell).toBeInTheDocument();
+
+    const rows = container.getElementsByClassName(ROW_CLASS);
+    const robotRow = rows[coordinateY];
+    const robotCells = robotRow.getElementsByClassName(CELL_CLASS);
+    const expectedRobotCell = robotCells[coordinateX];
+    expect(expectedRobotCell).toHaveClass(CELL_ROBOT_CLASS);
+  });
+
+  test('does not render robot in incorrect positions', () => {
+    const coordinateX = 2;
+    const coordinateY = 3;
+    const { container } = render(
+      <RobotPlayground
+        coordinateX={coordinateX}
+        coordinateY={coordinateY}
+        direction={Direction.NORTH}
+      />
+    );
+    const rows = container.getElementsByClassName(ROW_CLASS);
+    for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+      const cells = rows[rowIndex].getElementsByClassName(CELL_CLASS);
+      for (let colIndex = 0; colIndex < cells.length; colIndex++) {
+        if (rowIndex !== coordinateY || colIndex !== coordinateX) {
+          expect(cells[colIndex]).not.toHaveClass(CELL_ROBOT_CLASS);
+        }
+      }
+    }
+  });
+});
---
+++ src/components/robot-playground/RobotPlayground.tsx
@@ -0,0 +1,43 @@
+import { Robot } from 'src/components/robot/Robot';
+import { TOTAL_GRID_SIZE } from 'src/constants/direction.constant';
+import { Direction } from 'src/types/types';
+import './RobotPlayground.css';
+
+interface RobotSimulatorProps {
+  coordinateX: number;
+  coordinateY: number;
+  direction: Direction;
+}
+
+export const RobotPlayground = ({
+  coordinateX,
+  coordinateY,
+  direction,
+}: RobotSimulatorProps) => {
+  const isCellHaveRobot = (x: number, y: number) => {
+    return coordinateX === x && coordinateY === y;
+  };
+  return (
+    <div className='grid'>
+      {[...Array(TOTAL_GRID_SIZE)].map((_, rowIndex) => (
+        <div key={rowIndex} className='row'>
+          {[...Array(TOTAL_GRID_SIZE)].map((_, colIndex) => (
+            <div
+              key={colIndex}
+              className={`cell ${isCellHaveRobot(colIndex, rowIndex) ? 'cell__robot' : ''}`}
+            >
+              <Robot
+                direction={direction}
+                className={
+                  isCellHaveRobot(colIndex, rowIndex)
+                    ? 'cell__robot--visible'
+                    : 'cell__robot--invisible'
+                }
+              />
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
