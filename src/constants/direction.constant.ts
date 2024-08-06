66c2c4fc8af8f0f5a2c7d7a31381e999690d360f
Rishabh7g <rishabh7g@gmail.com>
2024-08-06 16:18:58 +1000

    Update 'types' location

--- src/constants/direction.constant.ts
+++ src/constants/direction.constant.ts
@@ -1,11 +1,11 @@
-import { Direction } from 'src/types';
+import { Direction } from 'src/types/types';
 
+export const LEFT_ROTATION = 3;
+export const RIGHT_ROTATION = 1;
+export const OPPOSITE_ROTATION = 2;
 export const DIRECTIONS: Direction[] = [
   Direction.NORTH,
   Direction.EAST,
   Direction.SOUTH,
   Direction.WEST,
 ];
-export const LEFT_ROTATION = 3;
-export const RIGHT_ROTATION = 1;
-export const OPPOSITE_ROTATION = 2;
--- src/types/types.ts
+++ src/types/types.ts
@@ -4,6 +4,11 @@ export interface Position {
   direction: Direction;
 }
 
+export enum TurnDirection {
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+}
+
 export enum Direction {
   NORTH = 'NORTH',
   EAST = 'EAST',
